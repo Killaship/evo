a3154c2e39d27edf7f9afaf613c2d8668a8d1e2c
Killaship <69988679+Killaship@users.noreply.github.com>
2022-04-04 06:33:53 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,11 +7,22 @@ int b1 = 100;
 int d1 = 10;
 int r1 = 5;
 
+void step() {
+	int randnum = randombytes.random() % 100 + 1;
+	if(randnum <= b1) {
+		population++;
+	}
+	if(randnum <= d1) {
+		population--;
+	}
+	
+}
 int main() {
 	if(sodium_init() < 0) {
 		/* panic! the library couldn't be initialized, it is not safe to use */
 		return 1;
 	}
+	step();
 
 }
   
