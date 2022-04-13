6dd4c0b6dee8b0254927e09cec4553987772e481
Killaship <killaship27@gmail.com>
2022-04-13 09:50:49 -0400

    a

--- src/main.c
+++ src/main.c
@@ -2,42 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int population;
-
-int b1 = 100;
-int d1 = 10;
-int r1 = 5;
-
+const int populationsize = 8;
+int population[populationsize];
+int cutoff = 1; // Provided in arguments, is minimum resistance needed to survive. Increments by 0.5 each generation.
+int m1 = 10;
 int randnum() {
 	int randnum;
 	randnum = randombytes_random() % 100 + 1;
 	return randnum;
 }
 void step() {
-	
 	if(randnum <= b1) {
 		population++;
 	}
-	for(int i = 1; i < population; i++) {
-		if(randnum() <= d1) {
-			population--;
-		}
-		if(randnum() <= r1) {
-			population++;
+	for(int i = 1; i < populationsize; i++) {
+		if(population[i] < cutoff)
+		if(randnum() <= m1) {
+			population[i]++;
 		}
 	}
-	printf("Population = %d\n",population);
-	
+	cutoff = cutoff + 0.5;
+	printf("Survived Population = %d\n",population);
 }
 int main(int argc, char *argv[]) {
-	population = atoi(argv[2]);
 	if(sodium_init() < 0) {
 		/* panic! the library couldn't be initialized, it is not safe to use */
 		return 1;
 	}
 	int g;
+	cutoff = atoi(argv[2]);
 	for(g = 0; g < atoi(argv[1]); g++) {
 		step();
 	}
 }
-  
+
