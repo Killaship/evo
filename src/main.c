271948a76ce424f0cf0656dd3683f5120f77a0d4
Killaship <killaship27@gmail.com>
2022-04-14 19:55:46 -0400

    0.0.1 I guess

--- src/main.c
+++ src/main.c
@@ -2,35 +2,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-const int populationsize = 8;
+#define populationsize 8
 int population[populationsize];
-int cutoff = 1; // Provided in arguments, is minimum resistance needed to survive. Increments by 0.5 each generation.
+float cutoff = 1; // Minimum resistance needed to survive. Increments by 0.5 each generation.
 int m1 = 10;
+int m2 = 10;
 int randnum() {
 	int randnum;
 	randnum = randombytes_random() % 100 + 1;
 	return randnum;
 }
 void step() {
-	if(randnum <= b1) {
-		population++;
-	}
-	for(int i = 1; i < populationsize; i++) {
-		if(population[i] < cutoff)
-		if(randnum() <= m1) {
+	for(int i = 0; i < populationsize; i++) {
+		printf("%d",population[i]);
+		if(population[i] == 0) {
+			// skip, this creature is dead
+		}
+		else if(population[i] < cutoff) {
+			population[i] = 0;
+		}
+		else if(randnum() <= m1) {
+			population[i]--;
+		}
+		else if(randnum() <= m2) {
 			population[i]++;
 		}
 	}
-	cutoff = cutoff + 0.5;
-	printf("Survived Population = %d\n",population);
+	cutoff = cutoff + 0.1;
+	int sum = 0;
+	for(int i = 0; i < populationsize; i++) {
+		if(population[i] > cutoff) {
+			sum++;
+		}
+	}
+	printf(" Survived Population = %d Cutoff - %3f\n", sum, cutoff);
+	char waitchar;
+	scanf("%c", &waitchar);
 }
 int main(int argc, char *argv[]) {
 	if(sodium_init() < 0) {
 		/* panic! the library couldn't be initialized, it is not safe to use */
 		return 1;
 	}
+	for(int i = 0; i < populationsize; i++) {
+		population[i] = randnum() / 25;
+	}
 	int g;
-	cutoff = atoi(argv[2]);
 	for(g = 0; g < atoi(argv[1]); g++) {
 		step();
 	}
